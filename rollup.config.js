e5143e247397dc705f78d269aa8e7b8152b4e643
talamcol <talamcol>
2022-08-30 13:40:14 +0200

    fix: test something with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -30,4 +30,9 @@ export default [
     ],
     external: ['react', 'react-dom'],
   },
+  {
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
 ];
