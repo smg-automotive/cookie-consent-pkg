35042b0337d6e510faa380f769a05b134a50125d
talamcol <talamcol>
2022-08-31 11:18:36 +0200

    fix: use vanilla JS to make it work with Helmet

--- src/OneTrustCookieConsentBanner.tsx
+++ src/OneTrustCookieConsentBanner.tsx
@@ -1,30 +1,35 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 
 type Props = {
   domainScript: string;
 };
 
 const OneTrustCookieConsentBanner: React.FC<Props> = ({ domainScript }) => {
-  const href = 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js';
+  const src = 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js';
 
-  return (
-    <>
-      <link rel="preload" href={href} as="script" />
-      <script
-        async
-        src={href}
-        data-domain-script={domainScript}
-        type="text/javascript"
-      />
-      <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          /* eslint-disable-next-line @typescript-eslint/naming-convention */
-          __html: 'function OptanonWrapper() { }',
-        }}
-      />
-    </>
-  );
+  const loadOneTrust = () => {
+    const preloadLink = document.createElement('link');
+    preloadLink.rel = 'preload';
+    preloadLink.href = src;
+    preloadLink.as = 'script';
+    document.head.appendChild(preloadLink);
+
+    const script = document.createElement('script');
+    script.src = src;
+    script.type = 'text/javascript';
+    script.dataset.domainScript = domainScript;
+    script.async = true;
+    document.head.appendChild(script);
+  };
+
+  useEffect(() => {
+    if (document) {
+      loadOneTrust();
+    }
+  }, []);
+
+  return null;
 };
 
 export default OneTrustCookieConsentBanner;
