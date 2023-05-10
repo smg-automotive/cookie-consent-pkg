c74dc6f666b2eca6aaa32c1799def290bbbc3246
AdzicMilos <milos.adzic.ma@gmail.com>
2023-05-10 15:13:16 +0200

    feat: add custom hooks for cookie consent

--- src/__tests__/CookieConsent.Test.tsx
+++ src/__tests__/CookieConsent.Test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { act, renderHook } from '@testing-library/react';
 
-import { CookieConsentContext, CookieConsentProvider } from '../CookieConsent';
+import { CookieConsentProvider, useCookieConsent } from '../CookieConsent';
 import { Category } from '../category';
 
 const wrapper = ({
@@ -31,12 +31,9 @@ describe('CookieConsent', () => {
   });
 
   it('sets the consent to strictly necessary if OneTrust is disabled', () => {
-    const { result } = renderHook(
-      () => React.useContext(CookieConsentContext),
-      {
-        wrapper: wrapper({ enabled: false }),
-      }
-    );
+    const { result } = renderHook(useCookieConsent, {
+      wrapper: wrapper({ enabled: false }),
+    });
     expect(result.current.consent).toEqual([Category.StrictlyNecessaryCookies]);
     expect(result.current.isLoaded).toEqual(false);
   });
@@ -45,12 +42,9 @@ describe('CookieConsent', () => {
     window.OnetrustActiveGroups = `${Category.PerformanceCookies},${Category.FunctionalCookies}`;
     /* eslint-disable-next-line @typescript-eslint/no-empty-function */
     window.OptanonWrapper = function () {};
-    const { result, rerender } = renderHook(
-      () => React.useContext(CookieConsentContext),
-      {
-        wrapper: wrapper({ enabled: true }),
-      }
-    );
+    const { result, rerender } = renderHook(useCookieConsent, {
+      wrapper: wrapper({ enabled: true }),
+    });
 
     act(() => {
       (window.OptanonWrapper as () => void)();
@@ -70,12 +64,9 @@ describe('CookieConsent', () => {
     window.OnetrustActiveGroups = `${Category.PerformanceCookies},${Category.FunctionalCookies}`;
     /* eslint-disable-next-line @typescript-eslint/no-empty-function */
     window.OptanonWrapper = function () {};
-    const { rerender } = renderHook(
-      () => React.useContext(CookieConsentContext),
-      {
-        wrapper: wrapper({ enabled: true, onOneTrustLoaded }),
-      }
-    );
+    const { rerender } = renderHook(useCookieConsent, {
+      wrapper: wrapper({ enabled: true, onOneTrustLoaded }),
+    });
 
     act(() => {
       (window.OptanonWrapper as () => void)();
@@ -90,12 +81,9 @@ describe('CookieConsent', () => {
 
   it('changes the consent if the user uses the preference center', () => {
     const onChange = jest.fn();
-    const { result, rerender } = renderHook(
-      () => React.useContext(CookieConsentContext),
-      {
-        wrapper: wrapper({ enabled: true, onConsentChanged: onChange }),
-      }
-    );
+    const { result, rerender } = renderHook(useCookieConsent, {
+      wrapper: wrapper({ enabled: true, onConsentChanged: onChange }),
+    });
 
     // simulate OneTrustLoaded
     act(() => {
@@ -123,12 +111,9 @@ describe('CookieConsent', () => {
   it('opens the OneTrust preference center', () => {
     const openPreferenceCenter = jest.fn();
     window.OneTrust = { ToggleInfoDisplay: openPreferenceCenter };
-    const { result } = renderHook(
-      () => React.useContext(CookieConsentContext),
-      {
-        wrapper: wrapper({ enabled: true }),
-      }
-    );
+    const { result } = renderHook(useCookieConsent, {
+      wrapper: wrapper({ enabled: true }),
+    });
     result.current.openPreferenceCenter();
     expect(openPreferenceCenter).toHaveBeenCalled();
   });
