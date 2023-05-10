44ddc61157428ed66be055af20a7974b4d0fed10
AdzicMilos <milos.adzic.ma@gmail.com>
2023-05-10 18:20:11 +0200

    chore: export hook

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import OneTrustCookieConsentBanner from './OneTrustCookieConsentBanner';
-import { CookieConsentContext, CookieConsentProvider } from './CookieConsent';
+import {
+  CookieConsentContext,
+  CookieConsentProvider,
+  useCookieConsent,
+} from './CookieConsent';
 import { Category } from './category';
 
 export {
   CookieConsentProvider,
   CookieConsentContext,
+  useCookieConsent,
   OneTrustCookieConsentBanner,
   Category,
 };
